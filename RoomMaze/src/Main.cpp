4eba6ade7e42f7bf066a3af1452c02a6e128c528
Maximilian Kinzel <maxkinzelmax@gmail.com>
2019-05-15 23:47:00 +0200

    add wall switch

--- RoomMaze/src/Main.cpp
+++ RoomMaze/src/Main.cpp
@@ -209,6 +209,9 @@ void initContent() {
 	renderObjects.push_back(room8);
 	Static3D *room9 = new Static3D("assets/objects/rooms/north_none_1.obj", shader, glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, 0.0f, -10.0f)), glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 	renderObjects.push_back(room9);
+
+	Static3D *wallSwitch = new Static3D("assets/objects/wallSwitch/wallSwitch.obj", shader, glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, 0.0f, -4.0f)));
+	renderObjects.push_back(wallSwitch);
 }
 
 GLFWwindow* initOpenGL() {
